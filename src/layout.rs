51c1f6c66839d98fca8199d861cbe0db135d6fc9
hyungupark <hyungu.1215.park@gmail.com>
2024-09-15 15:41:01 +0900

    define box types

--- src/layout.rs
+++ src/layout.rs
@@ -1,5 +1,7 @@
 //! Basic CSS block layout.
 
+use crate::style;
+
 /*
     The layout module takes the style tree and translates it into a bunch of rectangles in
     a two-dimensional space.
@@ -46,4 +48,62 @@ struct Dimensions {
     padding: EdgeSizes,
     border: EdgeSizes,
     margin: EdgeSizes,
+}
+
+
+/*
+    Block and Inline Layout
+
+    The CSS display property determines which type of box an element generates. CSS
+    defines several box types, each with its own layout rules. Here only going to talk
+    about two of them: "block" and "inline".
+
+    I'll use this bit of pseudo-HTML to illustrate the difference:
+        <container>
+            <a></a>
+            <b></b>
+            <c></c>
+            <d></d>
+        </container>
+ */
+
+/// A node in the layout tree.
+/*
+    The Layout Tree
+
+    The layout tree is a collection of boxes. A box has dimensions, and it may contain
+    child boxes.
+ */
+pub struct LayoutBox<'a> {
+    pub dimensions: Dimensions,
+    pub box_type: BoxType<'a>,
+    pub children: Vec<LayoutBox<'a>>,
+}
+
+/*
+    A box can be a block node, an inline node, or an anonymous block box. (This will
+    need to change when I implement text layout, because line wrapping can cause a
+    single inline node to split into multiple boxes. But it will do for now.)
+ */
+enum BoxType<'a> {
+    BlockNode(&'a style::StyledNode<'a>),
+    InlineNode(&'a style::StyledNode<'a>),
+    AnonymousBlock,
+}
+
+impl<'a> LayoutBox<'a> {
+    fn new(box_type: BoxType<'a>) -> LayoutBox {
+        LayoutBox {
+            box_type,
+            dimensions: Default::default(), // initially set all fields to 0.0
+            children: Vec::new(),
+        }
+    }
+
+    fn get_styled_node(&self) -> &'a style::StyledNode<'a> {
+        match self.box_type {
+            BoxType::BlockNode(node) | BoxType::InlineNode(node) => node,
+            BoxType::AnonymousBlock => panic!("Anonymous block box has no styled node"),
+        }
+    }
 }

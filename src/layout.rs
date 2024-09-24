40dd77d44fd637469b94dcfdffe7f4d321f0bb86
hyungupark <hyungu.1215.park@gmail.com>
2024-09-24 21:37:32 +0900

    add module "painting"

--- src/layout.rs
+++ src/layout.rs
@@ -29,7 +29,7 @@ use crate::{css, style};
 
 /// Position of the content area relative to the document origin:
 #[derive(Copy)]
-struct Rect {
+pub struct Rect {
     x: f32,
     y: f32,
     width: f32,
